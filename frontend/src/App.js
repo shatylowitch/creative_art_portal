cd9c95febab74acf1707ec598b7f01c6aa46a0a0
Micah Anderson <micaherollyonelanderson@gmail.com>
2022-06-07 17:57:33 +0200

    Gallery Filtration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,21 @@ import Posts from "./components/Posts";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import NotFound from "./components/NotFound";
-import Profile from "./components/Profile";
+import ProfileLayout from "./components/ProfileLayout";
 import Protected from "./components/Protected";
 import Gallery from "./components/Gallery";
 import Artists from "./components/Artists";
 
 function App() {
   return (
-    <div className="mh-100">
+    <div className="mh-100 appbackground">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Posts />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="protected" element={<Protected />}>
-            <Route path="profile" element={<Profile />} />
+            <Route path="profile" element={<ProfileLayout />} />
           </Route>
           <Route path="gallery" element={<Gallery />} />
           <Route path="artists" element={<Artists />} />
