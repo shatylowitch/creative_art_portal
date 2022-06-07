84a7c7d50a12336d80aca9cff4249c8a3db63197
Micah Anderson <micaherollyonelanderson@gmail.com>
2022-06-07 13:03:36 +0200

    Finish User Profile Page

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,12 +1,23 @@
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 function Profile() {
   const user = useSelector((state) => {
     return state.auth.user;
   });
 
-  axios.get(`http://localhost:3000/users/${user.id}`);
+  const [profile, setProfile] = useState();
+  console.log(user);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/auth/profile/${user.id}`)
+
+      .then((res) => setProfile(res.data))
+      .catch((err) => console.log(err));
+    console.log(profile);
+  }, []);
+
   return (
     <div>
       <h1>Welcome back to your Profile!</h1>
